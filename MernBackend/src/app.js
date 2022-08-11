f10350f8434dc9b7bf77b6d3883f303e033b7d32
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2022-08-11 22:34:59 +0530

    "Backend"

--- MernBackend/src/app.js
+++ MernBackend/src/app.js
@@ -15,8 +15,8 @@ const partials_path = path.join(__dirname, "../templates/partials");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use(express.static(static_path));
+
 app.set("view engine", "hbs");
 app.set("views", template_path);
 hbs.registerPartials(partials_path);
